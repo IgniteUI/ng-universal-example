1219635766e97b55f1dbc7cae475229dec9bce71
Zdravko Kolev <ZKolev@infragistics.com>
2019-10-21 17:57:20 +0300

    Add domino and XMLHttpRequest

--- server.ts
+++ server.ts
@@ -20,6 +20,39 @@ import 'zone.js/dist/zone-node';
 import * as express from 'express';
 import {join} from 'path';
 
+// Server-side DOM implementation
+const domino = require('domino');
+const fs = require('fs');
+const path = require('path');
+const template = fs
+  .readFileSync(path.join('dist/browser', 'index.html'))
+  .toString();
+const window = domino.createWindow(template);
+window.Object = Object;
+window.Math = Math;
+
+// Ignite UI browser objects abstractions
+(global as any).window = window;
+(global as any).document = window.document;
+(global as any).HTMLElement = window.HTMLElement;
+(global as any).HTMLElement.prototype.getBoundingClientRect = () => {
+    return {
+      left: '',
+      right: '',
+      top: '',
+      bottom: ''
+  };
+};
+
+// If using IgxIconService to register icons
+(global as any).XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+
+// Other optional depending on your application configuration
+(global as any).object = window.object;
+(global as any).navigator = window.navigator;
+(global as any).localStorage = window.localStorage;
+(global as any).DOMTokenList = window.DOMTokenList;
+
 // Express server
 const app = express();
 
